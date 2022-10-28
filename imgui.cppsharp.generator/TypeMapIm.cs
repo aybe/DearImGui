ddeb36bc834d8340a54be834bd55291c16c49f8c
aybe <aybe@users.noreply.github.com>
2022-10-29 00:31:46 +0200

    Map int parameters to proper enums

---
+++ imgui.cppsharp.generator/TypeMapIm.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using CppSharp.AST;
+using CppSharp.Types;
+using Type = CppSharp.AST.Type;
+
+namespace imgui.cppsharp.generator;
+
+internal abstract class TypeMapIm : TypeMap
+{
+    private const string Namespace = "global::imgui.NET";
+
+    public override Type CSharpSignatureType(TypePrinterContext ctx)
+    {
+        var attribute = GetType().GetCustomAttribute<TypeMapAttribute>() ?? throw new InvalidOperationException();
+        var description = $"{Namespace}.{attribute.Type}";
+        var customType = new CustomType(description);
+        return customType;
+    }
+}
