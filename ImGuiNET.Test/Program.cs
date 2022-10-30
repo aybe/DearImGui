333695b497ca2ec6acf9f09caa072c52f50f0780
aybe <aybe@users.noreply.github.com>
2022-10-30 02:54:26 +0100

    Upgrade our little test program

--- ImGuiNET.Test/Program.cs
+++ ImGuiNET.Test/Program.cs
@@ -1,9 +1,46 @@
-﻿namespace ImGuiNET.Test;
+﻿// ReSharper disable StringLiteralTypo
 
-internal class Program
+using System.Diagnostics;
+
+namespace ImGuiNET.Test;
+
+internal static class Program
 {
-    private static void Main(string[] args)
+    private static unsafe void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        ImGui.SetCurrentContext(ImGui.CreateContext(null)); // for glyph ranges
+
+        using (var io = ImGui.GetIO())
+        {
+            var ranges = io.Fonts.GlyphRangesDefault;
+
+            var atlas = new ImFontAtlas(); // TODO destroy
+
+            using (var font = atlas.AddFontFromFileTTF("Roboto-Regular.ttf", 12.0f, null, ref *ranges))
+            {
+                var build = atlas.Build();
+
+                Debug.Assert(build);
+            }
+
+            ImGui.DestroyContext(ImGui.GetCurrentContext());
+
+            var context = ImGui.CreateContext(atlas);
+
+            ImGui.SetCurrentContext(context);
+        }
+
+        using (var io = ImGui.GetIO())
+        {
+            io.DisplaySize = new ImVec2(100, 100);
+
+            ImGui.NewFrame();
+
+            var pOpen = true;
+
+            ImGui.ShowDemoWindow(ref pOpen);
+        }
+
+        ImGui.DestroyContext(ImGui.GetCurrentContext());
     }
 }
