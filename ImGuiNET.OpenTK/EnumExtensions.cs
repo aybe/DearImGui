86462a5937743a62993b08c859081140b6090880
aybe <aybe@users.noreply.github.com>
2022-11-01 00:41:28 +0100

    Create EnumExtensions.cs

---
+++ ImGuiNET.OpenTK/EnumExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace ImGuiNET.OpenTK;
+
+public static class EnumExtensions
+{
+    public static bool HasFlags<T>(this T value, T flags) where T : struct, Enum, IConvertible
+    {
+        var a = value.ToUInt64(null);
+        var b = flags.ToUInt64(null);
+        var c = (a & b) == b;
+
+        return c;
+    }
+}
