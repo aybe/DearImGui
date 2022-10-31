f66356d60e83c700e3277edeee58fa902b9e5547
aybe <aybe@users.noreply.github.com>
2022-11-01 00:36:50 +0100

    Create Disposable.cs

---
+++ ImGuiNET.OpenTK/Disposable.cs
@@ -0,0 +1,49 @@
+using JetBrains.Annotations;
+
+namespace ImGuiNET.OpenTK;
+
+[PublicAPI]
+public abstract class Disposable : IDisposable
+{
+    private bool IsDisposed { get; set; }
+
+    #region IDisposable Members
+
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+
+    #endregion
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (IsDisposed)
+        {
+            return;
+        }
+
+        DisposeNative();
+
+        if (disposing)
+        {
+            DisposeManaged();
+        }
+
+        IsDisposed = true;
+    }
+
+    protected virtual void DisposeManaged()
+    {
+    }
+
+    protected virtual void DisposeNative()
+    {
+    }
+
+    ~Disposable()
+    {
+        Dispose(false);
+    }
+}
