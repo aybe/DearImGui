2475fa02ea374168c65c90d2271f9aedf1e777ed
aybe <aybe@users.noreply.github.com>
2022-11-01 00:30:01 +0100

    Add ImGuiNET.Test.OpenTK project

---
+++ ImGuiNET.Test.OpenTK/MyGameWindow.cs
@@ -0,0 +1,35 @@
+﻿using OpenTK.Graphics.OpenGL;
+using OpenTK.Mathematics;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+
+namespace ImGuiNET.Test.OpenTK;
+
+internal sealed class MyGameWindow : GameWindow
+{
+    public MyGameWindow(GameWindowSettings gameWindowSettings, NativeWindowSettings nativeWindowSettings)
+        : base(gameWindowSettings, nativeWindowSettings)
+    {
+    }
+
+    private double ElapsedTime { get; set; }
+
+    protected override void OnUpdateFrame(FrameEventArgs args)
+    {
+        ElapsedTime += args.Time;
+    }
+
+    protected override void OnRenderFrame(FrameEventArgs args)
+    {
+        GL.ClearColor(Color4.FromHsv(new Vector4((float)(ElapsedTime % 10.0f / 10.0f), 1.0f, 1.0f, 1.0f)));
+        GL.Clear(ClearBufferMask.ColorBufferBit);
+        SwapBuffers();
+    }
+
+    protected override void OnResize(ResizeEventArgs e)
+    {
+        base.OnResize(e);
+
+        GL.Viewport(0, 0, e.Width, e.Height);
+    }
+}
---
+++ ImGuiNET.Test.OpenTK/Program.cs
@@ -0,0 +1,13 @@
+﻿using OpenTK.Windowing.Desktop;
+
+namespace ImGuiNET.Test.OpenTK;
+
+internal static class Program
+{
+    private static void Main(string[] args)
+    {
+        using var window = new MyGameWindow(GameWindowSettings.Default, NativeWindowSettings.Default);
+
+        window.Run();
+    }
+}
