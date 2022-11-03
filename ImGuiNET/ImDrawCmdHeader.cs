9beecd3b7226b20ebedc896c4562c3f832abaf08
aybe <aybe@users.noreply.github.com>
2022-11-03 07:00:12 +0100

    Create ImDrawCmdHeader.cs

---
+++ ImGuiNET/ImDrawCmdHeader.cs
@@ -0,0 +1,10 @@
+﻿namespace ImGuiNET;
+
+partial class ImDrawCmdHeader
+{
+    /// <inheritdoc />
+    public override string ToString()
+    {
+        return $"{nameof(ClipRect)}: {ClipRect}, {nameof(VtxOffset)}: {VtxOffset}, {nameof(TextureId)}: {TextureId}";
+    }
+}
