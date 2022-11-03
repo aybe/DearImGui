b53e653a5d31cb75fb5e30761b3b15163b72116b
aybe <aybe@users.noreply.github.com>
2022-11-03 07:00:05 +0100

    Create ImDrawCmd.cs

---
+++ ImGuiNET/ImDrawCmd.cs
@@ -0,0 +1,17 @@
+﻿namespace ImGuiNET;
+
+partial class ImDrawCmd
+{
+    public unsafe IntPtr GetTexID()
+    {
+        // this is an inlined function and therefore has no entry point
+        // TODO keep synchronized with value in header as it may change
+        return ((__Internal*)__Instance)->TextureId;
+    }
+
+    /// <inheritdoc />
+    public override string ToString()
+    {
+        return $"{nameof(ElemCount)}: {ElemCount}, {nameof(VtxOffset)}: {VtxOffset}, {nameof(IdxOffset)}: {IdxOffset}, {nameof(TextureId)}: {TextureId}";
+    }
+}
