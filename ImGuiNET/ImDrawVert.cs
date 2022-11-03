2720393f8d213f8d3f1f70ef80f8cf241e0a80a4
aybe <aybe@users.noreply.github.com>
2022-11-03 07:00:21 +0100

    Create ImDrawVert.cs

---
+++ ImGuiNET/ImDrawVert.cs
@@ -0,0 +1,10 @@
+﻿namespace ImGuiNET;
+
+partial class ImDrawVert // TODO struct
+{
+    /// <inheritdoc />
+    public override string ToString()
+    {
+        return $"{nameof(Pos)}: {Pos:F3}, {nameof(Uv)}: {Uv:F2}, {nameof(Col)}: 0x{Col:X8}";
+    }
+}
