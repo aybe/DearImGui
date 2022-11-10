d3240e1f8d30ffc5c73c0e705a5c768939d37e0f
aybe <aybe@users.noreply.github.com>
2022-11-10 05:06:30 +0100

    Add ImGuiKeyData.ToString

---
+++ imgui.NET/ImGuiKeyData.cs
@@ -0,0 +1,10 @@
+﻿namespace imgui.NET;
+
+partial class ImGuiKeyData
+{
+    /// <inheritdoc />
+    public override string ToString()
+    {
+        return $"{nameof(Down)}: {Down}, {nameof(DownDuration)}: {DownDuration}, {nameof(DownDurationPrev)}: {DownDurationPrev}, {nameof(AnalogValue)}: {AnalogValue}";
+    }
+}
