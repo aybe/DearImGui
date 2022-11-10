67456b4d8d97d2a344663f8786656a64475d395e
aybe <aybe@users.noreply.github.com>
2022-11-10 04:47:19 +0100

    Implement ImFont.GetDebugName as property

--- imgui.NET/ImFont.cs
+++ imgui.NET/ImFont.cs
@@ -5,7 +5,9 @@ namespace imgui.NET;
 [UsedImplicitly(ImplicitUseTargetFlags.WithMembers)]
 partial class ImFont
 {
-    /// <inheritdoc/>
+    public string DebugName => ConfigData != null ? new string(ConfigData.Name).TrimEnd('\0') : "<unknown>";
+
+    /// <inheritdoc />
     public override string ToString()
     {
         return $"{nameof(FontSize)}: {FontSize}, {nameof(Scale)}: {Scale}";
