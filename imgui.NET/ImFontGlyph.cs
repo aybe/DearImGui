7bdbfec72a0a6b549ac2741462c23ec6934524fc
aybe <aybe@users.noreply.github.com>
2022-11-10 04:38:00 +0100

    Add ImFontGlyph.ToString

---
+++ imgui.NET/ImFontGlyph.cs
@@ -0,0 +1,10 @@
+﻿namespace imgui.NET;
+
+partial class ImFontGlyph
+{
+    /// <inheritdoc />
+    public override string ToString()
+    {
+        return $"{nameof(Codepoint)}: {Codepoint}, {nameof(AdvanceX)}: {AdvanceX}";
+    }
+}
