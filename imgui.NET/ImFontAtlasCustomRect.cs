9315854570ef6481d3a8563e2090bd3a3a982d00
aybe <aybe@users.noreply.github.com>
2022-11-10 05:15:24 +0100

    Add ImFontAtlasCustomRect.ToString

--- imgui.NET/ImFontAtlasCustomRect.cs
+++ imgui.NET/ImFontAtlasCustomRect.cs
@@ -3,4 +3,11 @@
 partial class ImFontAtlasCustomRect
 {
     public bool IsPacked => X != 0xFFFF;
+
+    /// <inheritdoc />
+    public override string ToString()
+    {
+        return
+            $"{nameof(Width)}: {Width}, {nameof(Height)}: {Height}, {nameof(X)}: {X}, {nameof(Y)}: {Y}, {nameof(GlyphID)}: {GlyphID}, {nameof(GlyphAdvanceX)}: {GlyphAdvanceX}, {nameof(GlyphOffset)}: {GlyphOffset}";
+    }
 }
