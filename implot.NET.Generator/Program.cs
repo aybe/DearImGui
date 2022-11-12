2bf77ac521b8a36e7cd0c62e326d00dc20786f3d
aybe <aybe@users.noreply.github.com>
2022-11-12 06:10:07 +0100

    Initial implot generation

--- implot.NET.Generator/Program.cs
+++ implot.NET.Generator/Program.cs
@@ -1,2 +1,62 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Diagnostics.CodeAnalysis;
+using CppSharp;
+using CppSharp.AST;
+using CppSharp.Generators;
+
+ConsoleDriver.Run(new ImPlotLibrary { Enhanced = null });
+
+internal static class Constants
+{
+    public const string Namespace = "implot.NET";
+}
+
+[SuppressMessage("ReSharper", "StringLiteralTypo")]
+internal sealed class ImPlotLibrary : ILibrary
+{
+    public bool? Enhanced { get; init; }
+
+    #region ILibrary Members
+
+    public void Setup(Driver driver)
+    {
+        var options = driver.Options;
+
+        options.OutputDir = Enhanced.HasValue
+            ? Enhanced.Value
+                ? "NEW"
+                : "OLD"
+            : @"..\..\..\..\implot.NET";
+
+        options.GeneratorKind = GeneratorKind.CSharp;
+        options.GenerateFinalizers = true;
+#if DEBUG
+        options.GenerateDebugOutput = true;
+#endif
+        options.MarshalCharAsManagedChar = true;
+        options.Verbose = true;
+
+        var module = options.AddModule("implot");
+
+        module.OutputNamespace = Constants.Namespace;
+        module.IncludeDirs.Add(@"..\..\..\..\imgui\imgui");
+        module.IncludeDirs.Add(@"..\..\..\..\implot\implot");
+        module.Defines.Add("IMGUI_DISABLE_OBSOLETE_FUNCTIONS");
+        module.Defines.Add("IMGUI_DISABLE_OBSOLETE_KEYIO");
+        module.Headers.Add("implot.h");
+    }
+
+
+    public void SetupPasses(Driver driver)
+    {
+    }
+
+    public void Preprocess(Driver driver, ASTContext ctx)
+    {
+    }
+
+    public void Postprocess(Driver driver, ASTContext ctx)
+    {
+    }
+
+    #endregion
+}
