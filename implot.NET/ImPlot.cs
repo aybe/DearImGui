36cec54aed44f0e0119ae1ab69307793d9302592
aybe <aybe@users.noreply.github.com>
2022-11-15 23:44:58 +0100

    Implement loading of implot native library

---
+++ implot.NET/ImPlot.cs
@@ -0,0 +1,20 @@
+﻿using imgui.NET;
+using JetBrains.Annotations;
+
+namespace implot.NET;
+
+[UsedImplicitly]
+partial class ImPlot
+{
+    static ImPlot()
+    {
+        var path = "implot";
+
+        var image = SymbolResolver.LoadImage(ref path);
+
+        if (image == IntPtr.Zero)
+        {
+            throw new DllNotFoundException(path);
+        }
+    }
+}
