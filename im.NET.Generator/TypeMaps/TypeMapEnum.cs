c2ff6650e114981bfa0208123e2941f4eae3681e
aybe <aybe@users.noreply.github.com>
2022-11-16 22:03:50 +0100

    Refactor enum type maps and implement them for implot
    Update im.NET.Generator.csproj

--- im.NET.Generator/TypeMaps/TypeMapEnum.cs
+++ im.NET.Generator/TypeMaps/TypeMapEnum.cs
@@ -9,9 +9,13 @@ internal abstract class TypeMapEnum : TypeMap
 {
     public override Type CSharpSignatureType(TypePrinterContext ctx)
     {
-        var attribute = GetType().GetCustomAttribute<TypeMapAttribute>() ?? throw new InvalidOperationException();
-        var description = $"{Constants.ImGuiNamespace}.{attribute.Type}";
-        var customType = new CustomType(description);
+        var type = GetType();
+        
+        var attribute1 = type.GetCustomAttribute<TypeMapAttribute>() ?? throw new InvalidOperationException();
+        var attribute2 = type.GetCustomAttribute<TypeMapEnumNamespaceAttribute>() ?? throw new InvalidOperationException();
+
+        var customType = new CustomType($"global::{attribute2.Namespace}.{attribute1.Type}");
+
         return customType;
     }
 }
---
+++ im.NET.Generator/TypeMaps/TypeMapEnumNamespaceAttribute.cs
@@ -0,0 +1,12 @@
+﻿namespace im.NET.Generator.TypeMaps;
+
+[AttributeUsage(AttributeTargets.Class)]
+internal sealed class TypeMapEnumNamespaceAttribute : Attribute
+{
+    public TypeMapEnumNamespaceAttribute(string @namespace)
+    {
+        Namespace = @namespace;
+    }
+
+    public string Namespace { get; }
+}
