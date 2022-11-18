f7345bfe5ba81a10b51d6b9011c6dfa1c12d6252
aybe <aybe@users.noreply.github.com>
2022-11-19 00:09:03 +0100

    Create ImPlotContextScope.cs

---
+++ implot.NET/ImPlotContextScope.cs
@@ -0,0 +1,19 @@
+﻿namespace implot.NET;
+
+public readonly struct ImPlotContextScope : IDisposable
+{
+    private readonly ImPlotContext Context;
+
+    public ImPlotContextScope(ImPlotContext context)
+    {
+        Context = ImPlot.GetCurrentContext();
+
+        ImPlot.SetCurrentContext(context);
+    }
+
+    /// <inheritdoc />
+    public void Dispose()
+    {
+        ImPlot.SetCurrentContext(Context);
+    }
+}
