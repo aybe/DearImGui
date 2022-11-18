e8c7c416ab8e6f83100c0e95a9eaf892074fc52d
aybe <aybe@users.noreply.github.com>
2022-11-19 00:03:35 +0100

    Add ImPlotContext : IEquatable<ImPlotContext>

---
+++ implot.NET/ImPlotContext.cs
@@ -0,0 +1,62 @@
+﻿namespace implot.NET;
+
+partial class ImPlotContext : IEquatable<ImPlotContext>
+{
+    #region IEquatable<ImPlotContext> Members
+
+    /// <inheritdoc />
+    public bool Equals(ImPlotContext? other)
+    {
+        if (ReferenceEquals(null, other))
+        {
+            return false;
+        }
+
+        if (ReferenceEquals(this, other))
+        {
+            return true;
+        }
+
+        return __Instance.Equals(other.__Instance);
+    }
+
+    #endregion
+
+    /// <inheritdoc />
+    public override bool Equals(object? obj)
+    {
+        if (ReferenceEquals(null, obj))
+        {
+            return false;
+        }
+
+        if (ReferenceEquals(this, obj))
+        {
+            return true;
+        }
+
+        if (obj.GetType() != GetType())
+        {
+            return false;
+        }
+
+        return Equals((ImPlotContext)obj);
+    }
+
+    /// <inheritdoc />
+    public override int GetHashCode()
+    {
+        // ReSharper disable once NonReadonlyMemberInGetHashCode
+        return __Instance.GetHashCode();
+    }
+
+    public static bool operator ==(ImPlotContext? left, ImPlotContext? right)
+    {
+        return Equals(left, right);
+    }
+
+    public static bool operator !=(ImPlotContext? left, ImPlotContext? right)
+    {
+        return !Equals(left, right);
+    }
+}
