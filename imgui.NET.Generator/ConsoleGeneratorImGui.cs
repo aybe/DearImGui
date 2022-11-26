3d4b511995d7ef328a368d1393adcf36bcaa8cfc
aybe <aybe@users.noreply.github.com>
2022-11-27 00:10:31 +0100

    ConsoleGeneratorImGui: merge ProcessVectorClass with ProcessClasses

--- imgui.NET.Generator/ConsoleGeneratorImGui.cs
+++ imgui.NET.Generator/ConsoleGeneratorImGui.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.Immutable;
-using System.Diagnostics.CodeAnalysis;
 using System.Text.RegularExpressions;
 using im.NET.Generator;
 
@@ -30,8 +29,6 @@ internal sealed class ConsoleGeneratorImGui : ConsoleGenerator
     {
         ProcessSymbols(ref text);
 
-        ProcessVectorClass(ref text);
-
         base.Process(ref text);
     }
 
@@ -40,6 +37,18 @@ internal sealed class ConsoleGeneratorImGui : ConsoleGenerator
         // the symbols class has wrong visibility and lacks partial, fix it
 
         text = text.Replace("internal class imgui", "partial class imgui");
+
+        // hide ImVector namespace as internal class as it cannot be moved onto ImVector<T> because of CS7042
+
+        text = text.Replace(
+            "namespace ImVector",
+            "internal static partial class ImVector"
+        );
+
+        text = text.Replace(
+            ".__Symbols",
+            string.Empty
+        );
 
         base.ProcessClasses(ref text);
     }
@@ -59,22 +68,6 @@ internal sealed class ConsoleGeneratorImGui : ConsoleGenerator
             @"public\s+static\s+(\w+)\s+(_(?!_)\w+)",
             @"internal static $1 $2",
             RegexOptions.Multiline
-        );
-    }
-
-    [SuppressMessage("ReSharper", "StringLiteralTypo")]
-    private void ProcessVectorClass(ref string text)
-    {
-        // hide ImVector namespace as internal class as it cannot be moved onto ImVector<T> because of CS7042
-
-        text = text.Replace(
-            "namespace ImVector",
-            "internal static partial class ImVector"
-        );
-
-        text = text.Replace(
-            ".__Symbols",
-            string.Empty
         );
     }
 }
