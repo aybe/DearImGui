512751af7b6552c0a5a0a8eb378e5b69feeb778f
aybe <aybe@users.noreply.github.com>
2022-12-18 03:46:19 +0100

    Implement error-checking and configuration parameter for clean script

--- clean.cmd
+++ clean.cmd
@@ -1,13 +1,33 @@
 @echo off
 
 if not defined VSCMD_VER (
-    echo This script must be run inside Developer Command Prompt
-    goto :eof
+    echo %COLOR_WARN%This script must be run inside Developer Command Prompt%COLOR_NORM%
+    goto :end
 )
 
-MSBuild /t:Clean /p:Configuration=Debug /p:Platform="Any CPU"
-MSBuild /t:Clean /p:Configuration=Debug /p:Platform=x86
-MSBuild /t:Clean /p:Configuration=Debug /p:Platform=x64
-MSBuild /t:Clean /p:Configuration=Release /p:Platform="Any CPU"
-MSBuild /t:Clean /p:Configuration=Release /p:Platform=x86
-MSBuild /t:Clean /p:Configuration=Release /p:Platform=x64
+if /i "%1" == "debug" (
+    set BUILD_TYPE=Debug
+)
+
+if /i "%1" == "release" (
+    set BUILD_TYPE=Release
+)
+
+if not defined BUILD_TYPE (
+    echo Usage: %0 Debug^|Release
+    goto end
+)
+
+MSBuild /t:Clean /p:Configuration=%BUILD_TYPE% /p:Platform="Any CPU" || goto err
+MSBuild /t:Clean /p:Configuration=%BUILD_TYPE% /p:Platform=x86 || goto err
+MSBuild /t:Clean /p:Configuration=%BUILD_TYPE% /p:Platform=x64 || goto err
+
+goto end
+
+:err
+echo %COLOR_WARN%Aborting, last command failed with error code %ERRORLEVEL%%COLOR_NORM%
+
+:end
+set BUILD_TYPE=
+set COLOR_NORM=
+set COLOR_WARN=
