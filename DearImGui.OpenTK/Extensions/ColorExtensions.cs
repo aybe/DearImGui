bbeda2237f9ab4b8134bae061d50d9e3e321bc47
aybe <aybe@users.noreply.github.com>
2023-01-13 05:25:38 +0100

    Create ColorExtensions.cs

---
+++ DearImGui.OpenTK/Extensions/ColorExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Drawing;
+using System.Numerics;
+
+#pragma warning disable CS1591
+
+namespace DearImGui.OpenTK.Extensions;
+
+public static class ColorExtensions
+{
+    public static Vector4 ToVector4(this Color color)
+    {
+        const float f = 1.0f / 255.0f;
+
+        var vector4 = new Vector4(color.R * f, color.G * f, color.B * f, color.A * f);
+
+        return vector4;
+    }
+}
