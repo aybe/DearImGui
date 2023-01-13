db5746a0a1872587de0d42c07668674ceb96ed9a
aybe <aybe@users.noreply.github.com>
2023-01-13 03:04:18 +0100

    Add IMPLOT_AUTO and IMPLOT_AUTO_COL

--- DearImPlot/ImPlot.cs
+++ DearImPlot/ImPlot.cs
@@ -1,4 +1,6 @@
-﻿using DearImGui;
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Numerics;
+using DearImGui;
 using JetBrains.Annotations;
 
 namespace DearImPlot;
@@ -9,6 +11,20 @@ namespace DearImPlot;
 [UsedImplicitly(ImplicitUseTargetFlags.WithMembers)]
 partial class ImPlot
 {
+    /// <summary>
+    ///     Indicates variable should deduced automatically.<br />
+    ///     https://github.com/epezent/implot/blob/15e494b76a78b44ae2c1b76608ff9bc39a661409/implot.h#L65.
+    /// </summary>
+    [SuppressMessage("ReSharper", "InconsistentNaming")]
+    public const int IMPLOT_AUTO = -1;
+
+    /// <summary>
+    ///     Special color used to indicate that a color should be deduced automatically.
+    ///     https://github.com/epezent/implot/blob/15e494b76a78b44ae2c1b76608ff9bc39a661409/implot.h#L67.
+    /// </summary>
+    [SuppressMessage("ReSharper", "InconsistentNaming")]
+    public static readonly Vector4 IMPLOT_AUTO_COL = new(0, 0, 0, -1);
+
     static ImPlot()
     {
         var path = "implot";
